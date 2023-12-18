feae7b96d42418a6fbe39d3a1b368186f54b658c
ksy <ksoup3434@gmail.com>
2023-12-18 23:06:00 +0900

    Fix: 스웨거 api property 추가

--- src/user/dto/user.signup.dto.ts
+++ src/user/dto/user.signup.dto.ts
@@ -1,17 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, IsNotEmpty, Length } from 'class-validator';
 
 export class SignupDto {
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'duck99@gmail.com',
+    description: 'email',
+    required: true,
+  })
   email: string;
 
   @IsString()
   @IsNotEmpty()
   @Length(8, 20)
+  @ApiProperty({
+    example: '12345678',
+    description: 'password',
+    required: true,
+  })
   password: string;
 
   @IsString()
   @IsNotEmpty()
   @Length(2, 10)
+  @ApiProperty({
+    example: '오리',
+    description: 'nickname',
+    required: true,
+  })
   nickname: string;
 }
