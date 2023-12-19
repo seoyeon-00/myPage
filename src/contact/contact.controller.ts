f1e34a7127c6b948efa181c3dbae46b824406bd1
ksy <ksoup3434@gmail.com>
2023-12-20 04:36:38 +0900

    [Feat] contact 삭제 api

--- src/contact/contact.controller.ts
+++ src/contact/contact.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ContactService } from './contact.service';
 import { ContactDto } from './dto/contact.dto';
 import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
@@ -27,5 +35,15 @@ export class ContactController {
   @Post('/create')
   async createContact(@Body() contactDto: ContactDto) {
     return await this.contactService.create(contactDto);
+  }
+
+  @ApiOperation({
+    summary: '문의 삭제 API',
+    description: '문의 데이터베이스 삭제',
+  })
+  @UseGuards(JwtAuthGuard)
+  @Delete('/:id')
+  async deleteContact(@Param('id') id: string) {
+    return await this.contactService.delete(id);
   }
 }
